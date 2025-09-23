5e870938a41157fb036f6484ee9419ba92741bdf
Eduardo Madariaga <madariagaedu@gmail.com>
2025-09-23 14:50:52 -0300

    debugging

--- cucumber.config.js
+++ cucumber.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from '@cucumber/cucumber';
-
-export default defineConfig({
+export default {
   paths: ['features/**/*.feature'],
-  require: ['features/step_definitions/**/*.js'],
-  format: ['pretty', 'html:cucumber-report'],
+  import: ['features/step_definitions/**/*.js', 'features/support/**/*.js'],
+  format: ['progress', 'html:cucumber-report'],
   formatOptions: {
     html: {
       output: 'cucumber-report/index.html',
     },
   },
-  worldParameters: {
-    // This will be populated by Playwright test runner
-  },
-});
+};

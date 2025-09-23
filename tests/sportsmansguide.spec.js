9bb9e7f8d0f2fa0df4bc3cad24c00ef68f9fec59
Ernesto Albarez <ernestoalbrz@gmail.com>
2025-09-23 15:18:30 -0300

    add home and cart basic test

--- tests/sportsmansguide.spec.js
+++ tests/sportsmansguide.spec.js
@@ -1,10 +1,30 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/home.page.js';
 
-test('Navigate to homepage and verify #account-mmenu present', async ({ page, baseURL }) => {
-  const home = new HomePage(page, baseURL);
-  await home.goto();
-  await home.page.waitForSelector('#site-header');
+test.describe('Homepage actions', () => {
+  let home;
+  let searchTerm = 'U.S. Military Surplus Waterproof M2A1 .50 Caliber Ammo Can, Used';
+  //let searchTerm = 'U.S. Military Surplus Waterproof M2A1';
+
+  test.beforeEach(async ({ page, baseURL }) => {
+    home = new HomePage(page, baseURL);
+    await home.goto();
+  });
+
+  test('Navigate to homepage and verify #site-header is visible', async () => {
+    await expect(home.locators.siteHeader).toBeVisible();
+  });
+
+  test('Search for a term and verify search results appear', async () => {
+    await home.search(searchTerm);
+    await expect(home.locators.searchBar).toBeVisible();
+    await expect(home.locators.searchButton).toBeVisible();
+    await expect(home.locators.productCards.first()).toBeVisible();
+  });
+
+  test('Add item to cart and verify cart summary', async () => {
+    await home.search(searchTerm);
+    await home.addToCart();
+    await expect(home.locators.cartSummary).toBeVisible();
+  });
 });
-
-

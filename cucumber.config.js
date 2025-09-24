aee0143c378a9b1b082924d0aee643753b11166f
Eduardo Madariaga <madariagaedu@gmail.com>
2025-09-24 10:57:39 -0300

    fixing

--- cucumber.config.js
+++ cucumber.config.js
@@ -1,7 +1,7 @@
 export default {
   paths: ['features/**/*.feature'],
   import: ['features/step_definitions/**/*.js', 'features/support/**/*.js'],
-  format: ['progress', 'html:reports/cucumber-report.html'],
+  format: ['progress', 'html:reports/index.html'],
   timeout: 60000, // 60 seconds global timeout
   worldParameters: {
     // Pass timeout to world
